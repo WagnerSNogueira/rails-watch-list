8f6ff090b1285f4844eb8e24d7eff5dba6cc0992
wagnernogueira <wsnogueira@outlook.pt>
2022-08-18 18:06:41 -0300

    movies scss

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :list, through: :bookmarks
+
+  validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :overview, presence: true
+end
